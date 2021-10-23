b90448d27070035199ec7b1dae53d299d4938485
DarynaL <85280006+DarynaL@users.noreply.github.com>
2021-10-23 11:35:03 +0300

    Выполнено второе задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', clickOnGalleryItem);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    }).join('');
+}
+function clickOnGalleryItem(evt) {
+    evt.preventDefault();
+    const onItemUrl = evt.target;
+    console.log(onItemUrl);
+
+    
+    var lightbox = new SimpleLightbox('.gallery a', {
+        counterDelay: 250,
+        captionsData: 'alt',
+        showCounter: false,
+    });
+}
